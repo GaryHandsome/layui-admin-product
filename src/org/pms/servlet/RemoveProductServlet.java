93c80e5dd1f2a9709d9c4e48e32c9c4f0dc63201
zqx <114729040@qq.com>
2022-12-06 10:54:47 +0800

    完成了商品的删除

---
+++ src/org/pms/servlet/RemoveProductServlet.java
@@ -0,0 +1,19 @@
+package org.pms.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 删除商品
+ *
+ * @Date 2022-12-06
+ * @Author zqx
+ */
+public class RemoveProductServlet extends BaseServlet{
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // 删除商品....
+    }
+}
