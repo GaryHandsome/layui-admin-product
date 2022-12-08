2e1432f6b1b9797f1d152ec482b58176a0dcb694
zqx <114729040@qq.com>
2022-12-08 09:27:20 +0800

    实现了删除操作

--- src/org/pms/servlet/RemoveProductServlet.java
+++ src/org/pms/servlet/RemoveProductServlet.java
@@ -1,6 +1,9 @@
 package org.pms.servlet;
 
+import org.pms.dao.impl.ProductDaoImpl;
+
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -11,11 +14,18 @@ import java.io.IOException;
  * @Date 2022-12-06
  * @Author zqx
  */
+@WebServlet("/admin/RemoveProductServlet")
 public class RemoveProductServlet extends BaseServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 删除商品....
-        System.out.println("根据商品名称删除");
-        System.out.println("删除成功");
+        String productId = req.getParameter("pid");
+        ProductDaoImpl productDao = new ProductDaoImpl();
+        int row = productDao.delete(productId);
+
+        if(row==1) {
+            print(resp,successJson(200,"删除成功",""));
+            return ;
+        }
+        print(resp,errorJson(500,"删除失败",""));
     }
 }
